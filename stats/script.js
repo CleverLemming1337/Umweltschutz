67d02f17a5bb09f53a928107fe3f458e8ee3ca0b
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2023-01-08 12:45:01 +0100

    Add files via upload

---
+++ stats/script.js
@@ -0,0 +1,9 @@
+function info1() {
+    alert("So haben wir Bäume gepflanzt: \n - Für jede verkaufte Tafel Schokolade wurden 0,2 Bäume gepflanzt.");
+}
+function info2() {
+    alert("Wir haben die Einnahmen von Schokolade und Pflanzen dafür benutzt, den Bäumen zu helfen, indem wir: \n - Sie mit Wassersäcken ausgestattet haben.");
+}
+function info3() {
+    alert("So haben wir Geld verdient: \n - Schokolade und Pflanzen verkauft");
+}
